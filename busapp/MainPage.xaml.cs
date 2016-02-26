5e758505a9144cce6a042ad6c6fcca0bcb85c1da
Jose <jolguzma@ucsc.edu>
2016-02-25 16:25:04 -0800

    windows busit version

---
+++ busapp/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using busapp.Resources;
+using System.Device.Location;
+
+namespace busapp
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Loaded += MainPage_Loaded;
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        
+        void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            UpdateMap();
+        }
+
+
+
+        private void UpdateMap()
+        {
+            CampusMap.SetView(new GeoCoordinate(36.991406, -122.060731), 15);
+        }
+
+
+   
+    }
+}
