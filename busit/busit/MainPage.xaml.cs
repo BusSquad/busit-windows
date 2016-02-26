8412b3ec0da8dcd84dfaf342c3e8ffae7a8b855f
Jose <jolguzma@ucsc.edu>
2016-02-25 16:53:50 -0800

    working map

---
+++ busit/busit/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using busit.Resources;
+
+namespace busit
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Loaded += MainPage_Loaded;
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+
+        void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            UpdateMap();
+        }
+
+
+
+        private void UpdateMap()
+        {
+            CampusMap.SetView(new GeoCoordinate(36.991406, -122.060731), 15);
+        }
+
+    }
+}
