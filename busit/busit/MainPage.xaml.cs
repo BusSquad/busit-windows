ea174970eff40280bb0ec1f11e70aab48352ad54
Gerald Brown <gemabrow@ucsc.edu>
2016-02-26 23:38:50 -0800

    Added class for converting Geocoordinates to GeoCoordinates (yes, that's a thing). Added async'd user location method.

--- busit/busit/MainPage.xaml.cs
+++ busit/busit/MainPage.xaml.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Device.Location;      // Provides the GeoCoordinate class.
 using System.Linq;
+using System;
 using System.Net;
 using System.Windows;
-using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using Microsoft.Phone.Maps.Controls;
+using Windows.Devices.Geolocation;  // Provides the Geocoordinate class
 using busit.Resources;
-using System.Device.Location;
 
 namespace busit
 {
@@ -20,8 +22,6 @@ namespace busit
         public MainPage()
         {
             InitializeComponent();
-            Map OurMap = new Map();
-            ContentPanel.Children.Add(OurMap);
             Loaded += MainPage_Loaded;
 
             // Sample code to localize the ApplicationBar
@@ -32,14 +32,50 @@ namespace busit
         void MainPage_Loaded(object sender, RoutedEventArgs e)
         {
             UpdateMap();
+            ShowUserLocation();
         }
-
-
 
         private void UpdateMap()
         {
-            CampusMap.SetView(new GeoCoordinate(36.991406, -122.060731), 15);
+            CampusMap.SetView(new GeoCoordinate(36.991406, -122.060731), 14);
         }
 
+        private async void ShowUserLocation()
+        {
+            // Get my current location requires stupid conversions
+            Geolocator myGeolocator = new Geolocator();
+            Geoposition myGeoposition = await myGeolocator.GetGeopositionAsync();
+            Geocoordinate myGeocoordinate = myGeoposition.Coordinate;
+            GeoCoordinate userLocation =
+                CoordinateConverter.ConvertGeocoordinate(myGeocoordinate);
+
+            // Create a small circle to indicate location
+            Ellipse userCircle = new Ellipse();
+            userCircle.Fill = new SolidColorBrush(Colors.Blue);
+            userCircle.Height = 20;
+            userCircle.Height = 20;
+            userCircle.Width = 20;
+
+            // Create a MapOverlay to contain the circle.
+            MapOverlay myLocationOverlay = new MapOverlay();
+            myLocationOverlay.Content = userCircle;
+            myLocationOverlay.PositionOrigin = new Point(0.5, 0.5);
+            myLocationOverlay.GeoCoordinate = userLocation;
+
+            // Create a MapLayer to contain the MapOverlay.
+            MapLayer myLocationLayer = new MapLayer();
+            myLocationLayer.Add(myLocationOverlay);
+            // Add the MapLayer to the Map
+            CampusMap.Layers.Add(myLocationLayer);
+
+            // NOTE: The following is disabled due to Microsoft
+            // defaulting geolocations to their headquarters in
+            // Redmond, WA until obtaining a token for location
+            // information -- which is only available when
+            // submitting an app to their store.
+            // Center map on current location
+            //this.CampusMap.Center = userLocation;
+            //this.CampusMap.ZoomLevel = 13;
+        }
     }
 }
