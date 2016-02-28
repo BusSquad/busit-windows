74ee448c2518094bea2cf5bb1440b9afe2d07e3d
Gerald Brown <gemabrow@ucsc.edu>
2016-02-28 00:36:01 -0800

    Added WebClient Extensions. Possibly have a working WebClient now. TO-DO: Remove extraneous extensions.

--- busit/busit/BusTrackers.cs
+++ busit/busit/BusTrackers.cs
@@ -1,58 +1,37 @@
-﻿//using System;
-//using System.Diagnostics;
-//using System.Net;
-//using System.Threading.Tasks;
-//using System.Windows;
-//using WinPhoneExtensions;
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
 
-//namespace busit
-//{
-//    class bustrackers
-//    {
-//        private static string json_request = "http://bts.ucsc.edu:8081/location/get";
-//        private static string xml_request = "http://skynet.cse.ucsc.edu/bts/coord2.xml";
+namespace busit
+{
+    class BusTrackers
+    {
+        private Uri target;
+        private static string json_request = "http://bts.ucsc.edu:8081/location/get";
+        private static string xml_request = "http://skynet.cse.ucsc.edu/bts/coord2.xml";
 
-//        public async Task<string> GetBusTrackerDataAsync()
-//        {
-//            HttpWebRequest request = (HttpWebRequest)
-//                WebRequest.Create(json_request);
-//            request.Method = HttpMethod.Get;
-//            request.Accept = "application/json;odata=verbose";
+        public async Task<string> GetBusTrackerDataAsync()
+        {
+            return await GetBusTrackerDataAsync(0);
+        }
 
-//            try
-//            {
-//                HttpWebResponse response = (HttpWebResponse)
-//                    await request.GetResponseAsync();
+        public async Task<string> GetBusTrackerDataAsync(int arg)
+        {
+            switch (arg)
+            {
+                case 1:
+                    target = new Uri(xml_request);
+                    break;
+                case 0:
+                default:
+                    target = new Uri(json_request);
+                    break;
+            }
 
-//                Debug.WriteLine(response.ContentType);
+            var client = new WebClient();
+            string response = await client.DownloadStringTaskAsync(target);
+            return response;
+        }
 
-//                // Read the response into a Stream object.
-//                System.IO.Stream responseStream = response.GetResponseStream();
-//                string data;
-//                using (var reader = new System.IO.StreamReader(responseStream))
-//                {
-//                    data = reader.ReadToEnd();
-//                }
-//                responseStream.Close();
-
-//                //var feed =
-//                //    Newtonsoft.Json.JsonConvert.DeserializeObject<SupplierODataFeed>(data);
-//                //SuppliersList.ItemsSource = feed.d.results;
-//            }
-//            catch (Exception ex)
-//            {
-//                var we = ex.InnerException as WebException;
-//                if (we != null)
-//                {
-//                    var resp = we.Response as HttpWebResponse;
-//                    var code = resp.StatusCode;
-//                    MessageBox.Show("RespCallback Exception raised! Message:{0}" + we.Message);
-//                    Debug.WriteLine("Status:{0}", we.Status);
-//                }
-//                else
-//                    throw;
-//            }
-//        }
-//    }
-//}
-//}
+    }
+}
