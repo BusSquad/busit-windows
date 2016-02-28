0a81b0bd655341ebc45742aabe9b75d2a7764257
Gerald Brown <gemabrow@ucsc.edu>
2016-02-27 20:24:02 -0800

    Minor changes to Layout. Changed color scheme to match  UCSC's blue-and-gold.

---
+++ busit/busit/BusTrackers.cs
@@ -0,0 +1,22 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Net;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace busit
+//{
+//    class BusTrackers
+//    {
+//        private static string JSON_REQUEST = "http://bts.ucsc.edu:8081/location/get";
+//        private static string XML_REQUEST = "http://skynet.cse.ucsc.edu/bts/coord2.xml";
+
+//        async Task<String> AccessBusTrackers()
+//        {
+//            HttpClient client = new HttpClient();
+
+
+//        }
+//    }
+//}
